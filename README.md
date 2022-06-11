41a3755d5d04ff771f34816605ae83a558943f96
EnTeen <yas-2@yandex.ru>
2022-06-11 15:33:08 +0300

    Спринт 9.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# api_final
-api final
+# api_final_yatube
+## Описание
+Задача проекта:
+Дописать код и привести его в соответствие с документацией: добавить недостающие модели в приложении posts, создать адреса и представления для обработки запросов в приложении api.
+## Установка
+- Клонирование
+```sh
+https://github.com/EnTeen/api_final_yatube.git
+```
+```sh
+cd api_final_yatube
+```
+- Создание виртуального окружения
+```sh
+python -m venv env
+```
+```sh
+source env/bin/activate
+```
+```sh
+python -m pip install --upgrade pip
+```
+```sh
+Настройка зависимостей из файла requirements.txt
+```
+- Выполнитье миграции
+```sh
+python manage.py makemigrations
+```
+```sh
+python manage.py migrate
+```
+- Запустите проект
+```sh
+python manage.py runserver
+```
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,21 @@
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
 
 
-from posts.models import Comment, Post
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+        ref_name = 'ReadOnlyUsers'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +34,29 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(read_only=True,
+                            slug_field='username',
+                            default=CurrentUserDefault())
+
+    following = SlugRelatedField(slug_field='username',
+                                 queryset=User.objects.all(),
+                                 validators=[UniqueValidator(
+                                     queryset=User.objects.all())])
+
+    def validate_following(self, value):
+        user = self.context.get('request').user
+        if value == user:
+            raise serializers.ValidationError(
+                'User и following не могут быть одинаковыми.')
+        return value
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'))]
